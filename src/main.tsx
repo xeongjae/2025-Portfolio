614c115eb5a230e3531aa8eb51dc1577dffcdfa1
Kim sungjae <newsungjae0407@gmail.com>
2025-03-27 16:37:07 +0900

    FIX: 배포 에러 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import './index.scss';
+import React from "react";
+import "./index.scss";
 
-import { BrowserRouter } from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 
-import App from './App'
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
